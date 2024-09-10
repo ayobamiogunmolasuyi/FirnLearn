bfdcf6eadc0bc7ed681a28afb98db789660de7fc
ayobamiogunmolasuyi <125416471+ayobamiogunmolasuyi@users.noreply.github.com>
2024-09-10 18:50:00 +0200

    Create random_forest.py

---
+++ random_forest.py
@@ -0,0 +1,8 @@
+"""
+@author: Ayobami Ogunmolasuyi
+Thayer School of Engineering at Dartmouth College
+ayobami.o.ogunmolasuyi.th@dartmouth.edu
+
+FirnLearn: A Neural Network based approach to Firn Densification Modeling for Antarctica
+
+"""
